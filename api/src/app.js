a2a40bcf39c277404293237f47416ca97bbc9e3b
Jackira01 <nicolasberu27@gmail.com>
2025-05-01 10:37:40 -0500

    changed to cors

--- api/src/app.js
+++ api/src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');
+const { FRONT_URL } = process.env;
 //routes var
 const pokemon = require('./routes/pokemon.route');
 const filter = require('./routes/filter.route');
@@ -19,16 +21,8 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+server.use(cors({ origin: FRONT_URL, credentials: true }));
+
 
 //routes
 server.use('/pokemon', pokemon);
