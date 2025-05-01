59aa3aa47e65e53cc846838bea857a9b41bb8bed
Jackira01 <nicolasberu27@gmail.com>
2025-04-30 19:19:28 -0500

    backend config

--- api/index.js
+++ api/index.js
@@ -19,11 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const port = process.env.PGPORT || 3001;
+const port = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(port, () => {
-    console.log(`listening at ${port}`); // eslint-disable-line no-console
+    console.log(`conectado en el puerto ${port}`); // eslint-disable-line no-console
   });
 });
--- api/src/db.js
+++ api/src/db.js
@@ -2,17 +2,14 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DATABASE_URL } = process.env;
+const { DB_NAME, USER_DB, LOCALHOST, PASSWORD } = process.env;
 
-const sequelize = new Sequelize(DATABASE_URL, {
+const sequelize = new Sequelize(DB_NAME, USER_DB, PASSWORD, {
+  host: LOCALHOST,
+  dialect: 'mysql',
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
-
-/* const sequelize = new Sequelize(DATABASE_URL, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-}); */
 
 const basename = path.basename(__filename);
 
